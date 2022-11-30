e7cc1dc953da111127c3e793490302da977eed87
giovanni-iannaccone <80602545+giovanni-iannaccone@users.noreply.github.com>
2022-11-30 12:41:33 -0500

    Update install.sh

--- bash_file/install.sh
+++ bash_file/install.sh
@@ -4,25 +4,25 @@ red="\e[1;31m"
 yellow="\e[0;33m"
 
 clear
-echo "$blue ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ██╗███╗   ██╗ ██████╗          "
-echo "$blue ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██║████╗  ██║██╔════╝          "
-echo "$blue ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     ██║██╔██╗ ██║██║  ███╗         "
-echo "$blue ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██║██║╚██╗██║██║   ██║         "
-echo "$blue ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║╚██████╔╝██╗██╗██╗"
-echo "$blue ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝"
+echo -e "$blue ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ██╗███╗   ██╗ ██████╗          "
+echo -e "$blue ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██║████╗  ██║██╔════╝          "
+echo -e "$blue ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     ██║██╔██╗ ██║██║  ███╗         "
+echo -e "$blue ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██║██║╚██╗██║██║   ██║         "
+echo -e "$blue ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║╚██████╔╝██╗██╗██╗"
+echo -e "$blue ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝"
 
-echo "\n"
+echo
 
 #checking for python
-echo  "$yellow [ * ] Checking for python"
+echo -e "$yellow [ * ] Checking for python"
 which python > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
-    echo "$green [ ✔ ]::[Python]: found"
+    echo -e "$green [ ✔ ]::[Python]: found"
     sleep 1
 else
-    echo "$red [ X ]::[Python]: python not found"
+    echo -e "$red [ X ]::[Python]: python not found"
     sleep 1
-    echo "$yellow [!]::[Installing Module Python...]"
+    echo -e "$yellow [!]::[Installing Module Python...]"
     apt install python3
     apt install pip
     apt install pip2
@@ -30,12 +30,12 @@ else
 fi
 
 #installing requirements
-echo "$yellow [ * ] Installing requirements"
+echo -e "$yellow [ * ] Installing requirements"
 pip install -r ../requirements.txt
-echo "$blue [ ✔ ] Requirements installed"
+echo -e "$blue [ ✔ ] Requirements installed"
 sleep 1
 
-echo "$blue Done, press ENTER to continue"
+echo -e "$blue Done, press ENTER to continue"
 read
 
 exit 0
