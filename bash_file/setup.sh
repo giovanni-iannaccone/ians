5ba01032dc064f669eda3cacb312d55857d6b118
giovanni-iannaccone <80602545+giovanni-iannaccone@users.noreply.github.com>
2022-11-30 21:42:16 +0100

    Update setup.sh

--- bash_file/setup.sh
+++ bash_file/setup.sh
@@ -32,7 +32,7 @@ fi
 
 #checking for internet connection
 echo -e "$yellow [ * ] Checking for internet connection"
-if ping -q -c -l -W 1 8.8.8.1 > /dev/null;
+if ping -q -c 1 -W 1 8.8.8.8 > /dev/null;
 then
     echo -e "$green [ X ]::[Internet Connection]: ONLINE"
     sleep 0.5
