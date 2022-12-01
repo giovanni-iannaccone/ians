63bde4fa204926077456ad67de0df5455bebe38c
giovanni-iannaccone <80602545+giovanni-iannaccone@users.noreply.github.com>
2022-12-01 15:01:46 +0100

    Update update.sh

--- bash_file/update.sh
+++ bash_file/update.sh
@@ -1,25 +1,34 @@
 blue="\e[34m"
 
 clear
-echo  -e "$blue  ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗██╗███╗   ██╗ ██████╗          "
-echo  -e "$blue  ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝         "
-echo  -e "$blue  ██║   ██║██████╔╝██║  ██║███████║   ██║   ██║██╔██╗ ██║██║  ███╗         "
-echo  -e "$blue  ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██║██║╚██╗██║██║   ██║         "
-echo  -e "$blue  ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝██╗██╗██╗"
-echo  -e "$blue   ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝"
+echo  -e "$blue  ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗██╗███╗   ██╗ ██████╗           $default"
+echo  -e "$blue  ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝           $default"
+echo  -e "$blue  ██║   ██║██████╔╝██║  ██║███████║   ██║   ██║██╔██╗ ██║██║  ███╗          $default"
+echo  -e "$blue  ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██║██║╚██╗██║██║   ██║          $default"
+echo  -e "$blue  ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝██╗██╗██╗ $default"
+echo  -e "$blue   ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝ $default"
 
 #checking for internet connection
-echo -e "$yellow [ * ] Checking for internet connection"
+echo -e "$yellow [ * ] Checking for internet connection $default"
 if ping -q -c 1 -W 1 8.8.8.8 > /dev/null;
 then
-    echo -e "$green [ X ]::[Internet Connection]: ONLINE"
+    echo -e "$green [ ✔ ]::[Internet Connection]: ONLINE $default"
     sleep 0.5
 else
-    echo -e "$red [ ✔ ]::[Internet Connection]: OFFLINE"
+    echo -e "$red [ X ]::[Internet Connection]: OFFLINE $default"
     sleep 0.5
-    echo -e "$default"
     exit 1
 fi
+
+#checking for git
+echo -e "$yellow [ * ] Checking for git $default"
+if git > /dev/null;
+then
+    echo -e "$green [ ✔ ]::[git]: found $default"
+    sleep 0.5
+else
+    echo -e "$red [ X ]::[git] not found $default"
+    apt install git || pacman -S install git
 
 chmod +x /etc/
 chmod +x /usr/share/doc
