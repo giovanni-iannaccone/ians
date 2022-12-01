e21a2d2b6360e20b466728ab5cd2271fd956649f
giovanni-iannaccone <80602545+giovanni-iannaccone@users.noreply.github.com>
2022-12-01 14:50:08 +0100

    Update setup.sh

--- bash_file/setup.sh
+++ bash_file/setup.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 blue="\e[34m"
-default="\e[0m"
+default="\e[0;0m"
 green="\033[92m"
 red="\e[1;31m"
 yellow="\e[0;33m"
 
 clear
-echo -e "$blue   ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ "
-echo -e "$blue  ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ "
-echo -e "$blue  ██║     ███████║█████╗  ██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗"
-echo -e "$blue  ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║"
-echo -e "$blue  ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝██╗██╗██╗"
-echo -e "$blue   ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝"
-echo -e "$blue\n -------------------------- Giovanni Iannaccone --------------------------"
+echo -e "$blue   ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ $default"
+echo -e "$blue  ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ $default"
+echo -e "$blue  ██║     ███████║█████╗  ██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗ $default"
+echo -e "$blue  ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║ $default"
+echo -e "$blue  ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝██╗██╗██╗ $default"
+echo -e "$blue   ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝╚═╝ $default"
+echo -e "$blue\n -------------------------- Giovanni Iannaccone -------------------------- $default"
 
-echo -e "\n"
+echo -e
 sleep 1
 
 #checking for root user
-echo -e "$yellow [ * ] Checking for root user"
+echo -e "$yellow [ * ] Checking for root user $default"
 echo -e "$default [User: $USER]"
 if [[ $EUID -ne 0 ]]; then
-   echo -ne "$red [ x ]::[User]: ( no root )"
+   echo -ne "$red [ x ]::[User]: ( no root ) $default"
    sleep 0.5
    echo -e "$default"
    exit 1
 else
-    echo -e "$green [ ✔ ]::[User]: ROOT"
+    echo -e "$green [ ✔ ]::[User]: ROOT $default"
     sleep 0.5
 fi
 
 #checking for internet connection
-echo -e "$yellow [ * ] Checking for internet connection"
+echo -e "$yellow [ * ] Checking for internet connection $default"
 if ping -q -c 1 -W 1 8.8.8.8 > /dev/null;
 then
-    echo -e "$green [ X ]::[Internet Connection]: ONLINE"
+    echo -e "$green [ X ]::[Internet Connection]: ONLINE $default"
     sleep 0.5
 else
-    echo -e "$red [ ✔ ]::[Internet Connection]: OFFLINE"
+    echo -e "$red [ ✔ ]::[Internet Connection]: OFFLINE $default"
     sleep 0.5
     echo -e "$default"
     exit 1
 fi
 
-echo -e "\n$blue [-- Starting $ IANS $ --]"
+echo -e "\n$blue [-- Starting $ IANS $ --] $default"
 sleep 1
